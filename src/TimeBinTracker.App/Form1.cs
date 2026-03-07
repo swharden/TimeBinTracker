cb94259916d6c1b96147f1a716c24a0d06b791e9
Scott W Harden <swharden@gmail.com>
2026-03-06 20:47:33 -0500

    Manage using tray icon

--- src/TimeBinTracker.App/Form1.cs
+++ src/TimeBinTracker.App/Form1.cs
@@ -2,9 +2,27 @@ namespace TimeBinTracker.App;
 
 public partial class Form1 : Form
 {
+    private readonly NotifyIcon TrayIcon;
+    private readonly ContextMenuStrip TrayMenu;
+
     public Form1()
     {
         InitializeComponent();
+
+        TrayMenu = new ContextMenuStrip();
+        TrayMenu.Items.Add("Open", null, OnOpen);
+        TrayMenu.Items.Add("Exit", null, OnExit);
+
+        TrayIcon = new NotifyIcon
+        {
+            Text = "My App",
+            Icon = new Icon("icon.ico"),
+            ContextMenuStrip = TrayMenu,
+            Visible = true
+        };
+
+        TrayIcon.DoubleClick += OnOpen;
+        TrayIcon.MouseClick += OnTrayIconMouseClick;
     }
 
     private void Form1_Load(object sender, EventArgs e)
@@ -14,5 +32,50 @@ public partial class Form1 : Form
 
         Uploader up = new();
         richTextBox2.Text = up.GetPayload(da);
+    }
+
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+        if (e.CloseReason == CloseReason.UserClosing)
+        {
+            // just hide the window but keep it running in the system tray
+            e.Cancel = true;
+            Hide();
+        }
+        else
+        {
+            base.OnFormClosing(e);
+        }
+    }
+
+    private void OnTrayIconMouseClick(object? sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Left)
+        {
+            var method = typeof(NotifyIcon)
+                .GetMethod("ShowContextMenu",
+                    System.Reflection.BindingFlags.Instance |
+                    System.Reflection.BindingFlags.NonPublic);
+            method?.Invoke(TrayIcon, null);
+        }
+    }
+
+    private void OnOpen(object? sender, EventArgs e)
+    {
+        Show();
+        WindowState = FormWindowState.Normal;
+        BringToFront();
+    }
+
+    private void OnExit(object? sender, EventArgs e)
+    {
+        TrayIcon.Visible = false;
+        Application.Exit();
+    }
+
+    protected override void OnFormClosed(FormClosedEventArgs e)
+    {
+        TrayIcon.Dispose();
+        base.OnFormClosed(e);
     }
 }
