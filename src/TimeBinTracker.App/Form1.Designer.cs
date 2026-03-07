a130a95e09a8ff99e5e29d970562dcbf2557b294
Scott Harden <swharden@gmail.com>
2026-03-07 00:33:37 -0500

    App: use horizontal chart format

--- src/TimeBinTracker.App/Form1.Designer.cs
+++ src/TimeBinTracker.App/Form1.Designer.cs
@@ -29,26 +29,16 @@ partial class Form1
     private void InitializeComponent()
     {
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-        richTextBox1 = new RichTextBox();
         button1 = new Button();
         richTextBox2 = new RichTextBox();
         activityMonitor1 = new ActivityMonitor();
+        richTextBox3 = new RichTextBox();
         SuspendLayout();
-        // 
-        // richTextBox1
-        // 
-        richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
-        richTextBox1.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        richTextBox1.Location = new Point(12, 12);
-        richTextBox1.Name = "richTextBox1";
-        richTextBox1.Size = new Size(213, 466);
-        richTextBox1.TabIndex = 0;
-        richTextBox1.Text = "";
         // 
         // button1
         // 
         button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-        button1.Location = new Point(672, 354);
+        button1.Location = new Point(551, 564);
         button1.Name = "button1";
         button1.Size = new Size(129, 42);
         button1.TabIndex = 2;
@@ -59,28 +49,38 @@ partial class Form1
         // 
         richTextBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
         richTextBox2.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        richTextBox2.Location = new Point(231, 144);
+        richTextBox2.Location = new Point(12, 354);
         richTextBox2.Name = "richTextBox2";
-        richTextBox2.Size = new Size(570, 204);
+        richTextBox2.Size = new Size(668, 204);
         richTextBox2.TabIndex = 3;
         richTextBox2.Text = "";
         // 
         // activityMonitor1
         // 
-        activityMonitor1.Location = new Point(231, 12);
+        activityMonitor1.Location = new Point(12, 12);
         activityMonitor1.Name = "activityMonitor1";
         activityMonitor1.Size = new Size(286, 126);
         activityMonitor1.TabIndex = 4;
+        // 
+        // richTextBox3
+        // 
+        richTextBox3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        richTextBox3.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        richTextBox3.Location = new Point(12, 144);
+        richTextBox3.Name = "richTextBox3";
+        richTextBox3.Size = new Size(668, 204);
+        richTextBox3.TabIndex = 5;
+        richTextBox3.Text = "";
         // 
         // Form1
         // 
         AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(813, 490);
+        ClientSize = new Size(692, 621);
+        Controls.Add(richTextBox3);
         Controls.Add(activityMonitor1);
         Controls.Add(richTextBox2);
         Controls.Add(button1);
-        Controls.Add(richTextBox1);
         Icon = (Icon)resources.GetObject("$this.Icon");
         Name = "Form1";
         StartPosition = FormStartPosition.CenterScreen;
@@ -90,9 +90,8 @@ partial class Form1
     }
 
     #endregion
-
-    private RichTextBox richTextBox1;
     private Button button1;
     private RichTextBox richTextBox2;
     private ActivityMonitor activityMonitor1;
+    private RichTextBox richTextBox3;
 }
--- src/TimeBinTracker.App/Form1.cs
+++ src/TimeBinTracker.App/Form1.cs
@@ -31,14 +31,14 @@ public partial class Form1 : Form
             {
                 // TODO: account for daily rollover
                 DailyActivity.SetActive(TimeOnly.FromDateTime(DateTime.Now));
-                richTextBox1.Text = DailyActivity.ToChart();
+                richTextBox3.Text = DailyActivity.ToChartHorizontal();
             }
         };
     }
 
     private void Form1_Load(object sender, EventArgs e)
     {
-        richTextBox1.Text = DailyActivity.ToChart();
+        richTextBox3.Text = DailyActivity.ToChartHorizontal();
 
         Uploader up = new();
         richTextBox2.Text = up.GetPayload(DailyActivity);
