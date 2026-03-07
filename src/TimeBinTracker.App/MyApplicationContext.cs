f4eaf59c1561b7f93d0df5b981050724d0934407
Scott Harden <swharden@gmail.com>
2026-03-07 18:13:17 -0500

    Start hidden (tray only)

---
+++ src/TimeBinTracker.App/MyApplicationContext.cs
@@ -0,0 +1,7 @@
+﻿namespace TimeBinTracker.App;
+
+internal class MyApplicationContext : ApplicationContext
+{
+    private readonly Form1 _form = new() { ShowInTaskbar = false };
+    public new Form1 MainForm => _form;
+}
