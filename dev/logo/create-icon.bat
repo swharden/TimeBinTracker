876ab7668336fde4ba2c4a6006683a044d9e39ed
Scott Harden <swharden@gmail.com>
2026-03-06 23:37:03 -0500

    create white bg icon

--- dev/logo/create-icon.bat
+++ dev/logo/create-icon.bat
@@ -1,1 +1,3 @@
-magick transparent.png -background transparent -define icon:auto-resize=16,24,32,48,64,72,96,128,256 icon.ico
+magick transparent.png -background transparent -define icon:auto-resize=16,24,32,48,64,72,96,128,256 transparent.ico
+magick transparent-white.png -background transparent -define icon:auto-resize=16,24,32,48,64,72,96,128,256 transparent-white.ico
+
