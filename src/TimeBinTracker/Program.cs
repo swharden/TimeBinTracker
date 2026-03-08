5c3a3ab4576104f5c55470df0102bcee883eca3f
Scott Harden <swharden@gmail.com>
2026-03-08 19:10:27 -0400

    Program: import all data

--- src/TimeBinTracker/Program.cs
+++ src/TimeBinTracker/Program.cs
@@ -1,3 +1,7 @@
 ﻿using TimeBinTracker;
 
-Report.Generate("converted");
+string importFrom = @"C:\Users\sharden\source\repos\ActiveWindowLogger\BUILD2\logs";
+string outputFolder = Path.GetFullPath("converted");
+Directory.Delete(outputFolder, true);
+Import.ActiveWindowLogger(importFrom, outputFolder);
+Report.Generate(outputFolder);
