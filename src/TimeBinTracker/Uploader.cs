832ac21bf192dc1949d504e6e16b4a8ab6b7432e
Scott Harden <swharden@gmail.com>
2026-03-08 11:01:23 -0400

    fix HTTP upload

--- src/TimeBinTracker/Uploader.cs
+++ src/TimeBinTracker/Uploader.cs
@@ -37,42 +37,24 @@ public class Uploader
 
     public async Task<HttpStatusCode> Upload(DayActivity day)
     {
-        HttpClientHandler handler = new()
-        {
-            AutomaticDecompression = DecompressionMethods.All
-        };
-
-        var client = new HttpClient(handler);
-
-        client.DefaultRequestHeaders.Add("User-Agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-            "AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36");
-        client.DefaultRequestHeaders.Add("Accept", "application/json, text/html, */*");
-        client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9");
-        client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
-
-        client.DefaultRequestHeaders.Add("Referer", "https://swharden.com/");
-        client.DefaultRequestHeaders.Host = "swharden.com";
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", SettingSecret);
 
         string url = SettingURL +
             $"?ComputerId={SettingComputer}" +
             $"&Day={day.GetDayCode()}" +
             $"&Hex={day.ToHex()}" +
-            $"&NoCache={Random.Shared.Next()}";
-
-        url = $"https://swharden.com/activity/add/test.html?{DateTime.Now.Ticks}";
+            $"&Timestamp={DateTime.UtcNow:O}";
 
         var res = await client.GetAsync(url);
-        string body = await res.Content.ReadAsStringAsync();
-
-        Debug.WriteLine("Body: " + body);
-        Debug.WriteLine("Status: " + res.StatusCode);
-        Debug.WriteLine("Headers: " + res.Headers);
-        Debug.WriteLine("Content Headers: " + res.Content.Headers);
 
         if (!res.IsSuccessStatusCode)
         {
-            Debug.WriteLine("");
+            Debug.WriteLine("Status: " + res.StatusCode);
+            Debug.WriteLine("Body: " + await res.Content.ReadAsStringAsync());
+            Debug.WriteLine("Headers: " + res.Headers);
+            Debug.WriteLine("Content Headers: " + res.Content.Headers);
         }
 
         return res.StatusCode;
--- src/TimeBinTracker/Version.cs
+++ src/TimeBinTracker/Version.cs
@@ -2,5 +2,6 @@
 
 public static class Version
 {
-    public static readonly string Number = "0.2";
+    public static readonly string Number = "0.3";
+    public static readonly string NameAndNumber = "TimeBinTracker v0.3";
 }
