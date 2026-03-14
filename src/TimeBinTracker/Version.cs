a7671b3f82bcdbbb88a3e7a89498c7cef9b5e8f1
Scott W Harden <swharden@gmail.com>
2026-03-14 17:24:02 -0400

    bump version

--- src/TimeBinTracker/Version.cs
+++ src/TimeBinTracker/Version.cs
@@ -2,6 +2,6 @@
 
 public static class Version
 {
-    public static readonly string Number = "0.3";
-    public static readonly string NameAndNumber = "TimeBinTracker v0.3";
+    public static readonly string Number = "0.4";
+    public static readonly string NameAndNumber = $"TimeBinTracker v{Number}";
 }
